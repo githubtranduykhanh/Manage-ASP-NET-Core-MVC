3cd1c4a706c33460f291366e07a6fec15478428e
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-05-18 00:22:04 +0700

    Update gitignore

--- ECommerceMVC/ECommerceMVC/obj/Release/net8.0/ECommerceMVC.AssemblyInfo.cs
+++ ECommerceMVC/ECommerceMVC/obj/Release/net8.0/ECommerceMVC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceMVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a2ec90398884862458e11ad32ad0469620ade1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceMVC")]
