c183282ed193b7989cc1581548e3fc0ce92ad7bb
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-05-18 17:23:29 +0700

    init 2

--- ECommerceMVC/ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
+++ ECommerceMVC/ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30f27ea50aa93a3dfbdc879fa3e07616427f5b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bacdb1957056a79267e281be7419e3549cdf01")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
