303b3fa9e2bdf2a3ce68082f24153ccf0f80a72b
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-06-01 18:17:39 +0700

    Table User Format Print...

--- ECommerceMVC/ECommerceMVC/wwwroot/js/adminPages/managerUsers.js
+++ ECommerceMVC/ECommerceMVC/wwwroot/js/adminPages/managerUsers.js
@@ -3,12 +3,164 @@
 
 
 const managerUser = (() => {
-    let tableUsers
-
+    let tableUsers, offCanvasEl
+    
     const managerUserStore = {
+
         init() {
             tableUsers = new DataTable('#js-table-users', {
                 responsive: true,
+                dom: '<"card-header flex-column flex-md-row"<"head-label text-center"><"dt-action-buttons text-end pt-3 pt-md-0"B>><"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6 d-flex justify-content-center justify-content-md-end"f>>t<"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>', // B: Buttons, f: Filter input, r: Processing, t: Table, i: Table information, p: Pagination
+                buttons: [{
+                    extend: "collection",
+                    className: "btn btn-label-primary dropdown-toggle me-2",
+                    text: '<i class="bx bx-export me-sm-1"></i> <span class="d-none d-sm-inline-block">Export</span>',
+                    buttons: [{
+                        extend: "print",
+                        text: '<i class="bx bx-printer me-1" ></i>Print',
+                        className: "dropdown-item",
+                        title: 'User List', // Tiêu đề của tài liệu in
+                        filename: function () {
+                            var timestamp = Math.floor(Date.now() / 1000); // Lấy thời gian hiện tại tính bằng giây
+                            return 'User List - ' + timestamp;
+                        },
+                        exportOptions: {
+                            columns: [1,2,3, 4, 5, 6, 7,8,9],
+                            format: {
+                                //body: function (e, t, a) {
+                                //    var s;
+                                //    return e.length <= 0 ? e : (e = $.parseHTML(e),
+                                //        s = "",
+                                //        $.each(e, function (e, t) {
+                                //            void 0 !== t.classList && t.classList.contains("user-name") ? s += t.lastChild.firstChild.textContent : void 0 === t.innerText ? s += t.textContent : s += t.innerText
+                                //        }),
+                                //        s)
+                                //}
+                                body: function (data, row, column, node) {
+                                    // Trả về giá trị dữ liệu của cột
+                                    return (column === 3 || column === 1) ? ($(node).attr('data-original-value') || data) : ($(node).find(':input').val() || data)                                                                
+                                }
+                                                            
+                            }
+                        },
+                        customize: function (e) {
+                            $(e.document.body).css("color", config.colors.headingColor).css("border-color", config.colors.borderColor).css("background-color", config.colors.bodyBg),
+                                $(e.document.body).find("table").addClass("compact").css("color", "inherit").css("border-color", "inherit").css("background-color", "inherit")
+                        }
+                    }, {
+                        extend: "csv",
+                        text: '<i class="bx bx-file me-1" ></i>Csv',
+                        className: "dropdown-item",
+                        title: 'User List', // Tiêu đề của tài liệu in
+                        filename: function () {
+                            var timestamp = Math.floor(Date.now() / 1000); // Lấy thời gian hiện tại tính bằng giây
+                            return 'User List - ' + timestamp;
+                        },
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                            format: {
+                                //body: function (e, t, a) {
+                                //    var s;
+                                //    return e.length <= 0 ? e : (e = $.parseHTML(e),
+                                //        s = "",
+                                //        $.each(e, function (e, t) {
+                                //            void 0 !== t.classList && t.classList.contains("user-name") ? s += t.lastChild.firstChild.textContent : void 0 === t.innerText ? s += t.textContent : s += t.innerText
+                                //        }),
+                                //        s)
+                                //}
+
+                                body: function (data, row, column, node) {
+                                    // Trả về giá trị dữ liệu của cột
+                                    return (column === 3 || column === 1) ? ($(node).attr('data-original-value') || data) : ($(node).find(':input').val() || data)  
+                                }
+                            }
+                        }
+                    }, {
+                        extend: "excel",
+                        text: '<i class="bx bxs-file-export me-1"></i>Excel',
+                        className: "dropdown-item",
+                        title: 'User List', // Tiêu đề của tài liệu in
+                        filename: function () {
+                            var timestamp = Math.floor(Date.now() / 1000); // Lấy thời gian hiện tại tính bằng giây
+                            return 'User List - ' + timestamp;
+                        },
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                            format: {
+                                //body: function (e, t, a) {
+                                //    var s;
+                                //    return e.length <= 0 ? e : (e = $.parseHTML(e),
+                                //        s = "",
+                                //        $.each(e, function (e, t) {
+                                //            void 0 !== t.classList && t.classList.contains("user-name") ? s += t.lastChild.firstChild.textContent : void 0 === t.innerText ? s += t.textContent : s += t.innerText
+                                //        }),
+                                //        s)
+                                //}
+                                body: function (data, row, column, node) {
+                                    // Trả về giá trị dữ liệu của cột
+                                    return (column === 3 || column === 1) ? ($(node).attr('data-original-value') || data) : ($(node).find(':input').val() || data)  
+                                }
+                            }
+                        }
+                    }, {
+                        extend: "pdf",
+                        text: '<i class="bx bxs-file-pdf me-1"></i>Pdf',
+                        className: "dropdown-item",
+                        title: 'User List', // Tiêu đề của tài liệu in
+                        filename: function () {
+                            var timestamp = Math.floor(Date.now() / 1000); // Lấy thời gian hiện tại tính bằng giây
+                            return 'User List - ' + timestamp;
+                        },
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                            format: {
+                                //body: function (e, t, a) {
+                                //    var s;
+                                //    return e.length <= 0 ? e : (e = $.parseHTML(e),
+                                //        s = "",
+                                //        $.each(e, function (e, t) {
+                                //            void 0 !== t.classList && t.classList.contains("user-name") ? s += t.lastChild.firstChild.textContent : void 0 === t.innerText ? s += t.textContent : s += t.innerText
+                                //        }),
+                                //        s)
+                                //}
+                                body: function (data, row, column, node) {
+                                    // Trả về giá trị dữ liệu của cột
+                                    return (column === 3 || column === 1) ? ($(node).attr('data-original-value') || data) : ($(node).find(':input').val() || data)  
+                                }
+                            }
+                        }
+                    }, {
+                        extend: "copy",
+                        text: '<i class="bx bx-copy me-1" ></i>Copy',
+                        className: "dropdown-item",
+                        title: 'User List', // Tiêu đề của tài liệu in
+                        filename: function () {
+                            var timestamp = Math.floor(Date.now() / 1000); // Lấy thời gian hiện tại tính bằng giây
+                            return 'User List - ' + timestamp;
+                        },
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                            format: {
+                                //body: function (e, t, a) {
+                                //    var s;
+                                //    return e.length <= 0 ? e : (e = $.parseHTML(e),
+                                //        s = "",
+                                //        $.each(e, function (e, t) {
+                                //            void 0 !== t.classList && t.classList.contains("user-name") ? s += t.lastChild.firstChild.textContent : void 0 === t.innerText ? s += t.textContent : s += t.innerText
+                                //        }),
+                                //        s)
+                                //}
+                                body: function (data, row, column, node) {
+                                    // Trả về giá trị dữ liệu của cột
+                                    return (column === 3 || column === 1) ? ($(node).attr('data-original-value') || data) : ($(node).find(':input').val() || data)  
+                                }
+                            }
+                        }
+                    }]
+                }, {
+                    text: '<i class="bx bx-plus me-sm-1"></i> <span class="d-none d-sm-inline-block">Add New Record</span>',
+                    className: "create-new btn btn-primary"
+                }],
                 columnDefs: [{
                     targets: 4, // index của cột "Address"
                     render: function (data, type, row) {
@@ -26,7 +178,33 @@ const managerUser = (() => {
                         return data;
                     }   
                 }]
-            });
+            })
+            const headLabel = document.querySelector('div.head-label')
+            headLabel.innerHTML = '<h5 class="card-title mb-0">DataTable User</h5>'
+            setTimeout(() => {
+                const e = document.querySelector(".create-new")
+                    , t = document.querySelector("#add-new-record");
+                e && e.addEventListener("click", function () {
+                    offCanvasEl = new bootstrap.Offcanvas(t),
+                        t.querySelector(".dt-full-name").value = "",
+                        t.querySelector(".dt-post").value = "",
+                        t.querySelector(".dt-email").value = "",
+                        t.querySelector(".dt-date").value = "",
+                        t.querySelector(".dt-salary").value = "",
+                        offCanvasEl.show()
+                })
+            }, 200) 
+            const dataTimeInputs = document.querySelectorAll(".dt-date")
+            Array.from(dataTimeInputs).forEach(input => { 
+                flatpickr(input, {
+                    enableTime: true,
+                    dateFormat: "m/d/Y H:i",
+                    time_24hr: false, // Sử dụng định dạng 12 giờ (AM/PM), đổi thành true nếu muốn 24 giờ
+                    monthSelectorType: "static",
+                    altInput: true,
+                    altFormat: "F j, Y",
+                });
+            })
         }
     }
     return managerUserStore
